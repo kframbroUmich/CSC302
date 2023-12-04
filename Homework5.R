64a406c3f50e494cd710993841b3d566b9048996
kframbroUmich <149113239+kframbroUmich@users.noreply.github.com>
2023-12-03 20:04:24 -0500

    Add files via upload

--- Homework5.R
+++ Homework5.R
@@ -3,24 +3,22 @@ library(ggplot2)
 library(tidyverse)
 library(lubridate)
 #1.A
-house_prices$year <-format(as.Date(house_prices$date, format="%Y-%d-%m"),"%Y")
-df <- house_prices
-ggplot(df, aes(x = year, y = house_price_index, color=state)) +
+house_prices <- house_prices %>%
+  mutate(year = as.numeric(format(date, "%Y")))
+ggplot(df, aes(x = as.factor(year), y = house_price_index, color=state)) +
   geom_line() +
-  facet_wrap(~ state, nrow=1) + 
-  scale_x_discrete(breaks = c('1980','2000','2020'),label=c('80','00','20')) + 
+  facet_wrap(~ state,nrow=1) + 
+  scale_x_discrete(breaks = c(1980,2000,2020),label=c('80','00','20')) + 
   labs(x = "Year", y = "House Price Index", title = "House Price Index Trend by State")
 #1.B
+house_reshaped <- house_prices %>% gather(measure,value, -date, -state,-house_price_perc, -year)
 
-house_prices$year <- format(as.Date(house_prices$date, format="%Y-%d-%m"),"%Y")
-house_prices
-
-ggplot(house_prices, aes(x=year, y=house_price_index, color=state)) + 
-  geom_line() +
-  facet_wrap(~ state, nrow = 1) +
-  scale_x_discrete(breaks=c('1980','2000','2020'), labels=c('80', '00', '20'))
 
 #1.C
+ggplot(house_reshaped, aes(x=year, y=value, color=state)) + 
+  geom_line() +
+  facet_wrap(~ measure, ncol = 1) +
+  scale_x_discrete(breaks=c(1980,2000,2020), labels=c('80', '00', '20'))
 
 #2
 house_prices$year <- format(as.Date(house_prices$date, format="%Y-%d-%m"),"%Y")
